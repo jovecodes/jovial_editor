53b90b1d12ff0625f4bda0d40eb5f06890e6e51f
jovecodes <86496060+jovecodes@users.noreply.github.com>
2023-04-01 19:11:12 -0700

    finished the input system

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -22,30 +22,74 @@ impl Plugin for Input {
 }
 
 impl Input {
+    pub fn is_pressed(&mut self, buttons: &Vec<Button>, mode: Modes) -> bool {
+        if self.mode != mode {
+            return false;
+        }
+        if vec_contains_vec(&self.buttons_pressed, buttons) {
+            self.buttons_pressed.clear();
+            true
+        } else {
+            false
+        }
+    }
+
     fn append(&mut self, buttons: Vec<Button>) {
         if buttons.is_empty() {
             return;
         }
         for i in buttons {
-            if i == Button::Escape {
-                self.mode = Modes::Normal;
-            }
-            if i == Button::I {
-                self.mode = Modes::Insert;
-            }
-            if i == Button::V {
-                self.mode = Modes::Visual;
-            }
-            if i == Button::Semicolon && self.shift {
-                self.mode = Modes::Command;
+            self.update_mode(&i);
+            if self.deal_with_backspace(&i) {
+                return;
             }
 
             if i == Button::LShift || i == Button::RShift {
                 self.shift = true;
+                self.buttons_pressed.push(Button::RShift);
+            } else if i == Button::LControl || i == Button::RControl {
+                self.buttons_pressed.push(Button::RControl);
+            } else {
+                self.buttons_pressed.push(i);
             }
-            self.buttons_pressed.push(i);
         }
         self.last_updated = Instant::now();
+    }
+
+    fn deal_with_backspace(&mut self, button: &Button) -> bool {
+        if button == &Button::Back && self.mode == Modes::Command {
+            self.buttons_pressed.pop();
+            if self.buttons_pressed.is_empty() {
+                self.mode = Modes::Normal;
+            }
+            return true
+        }
+
+        return false;
+    }
+
+    fn update_mode(&mut self, button: &Button) -> bool {
+        if button == &Button::Escape {
+            self.mode = Modes::Normal;
+            self.buttons_pressed.clear();
+            return true;
+        }
+        if button == &Button::I {
+            self.mode = Modes::Insert;
+            self.buttons_pressed.clear();
+            return true;
+        }
+        if button == &Button::V {
+            self.mode = Modes::Visual;
+            self.buttons_pressed.clear();
+            return true;
+        }
+        if button == &Button::Semicolon && self.shift {
+            self.mode = Modes::Command;
+            self.buttons_pressed.clear();
+            return true;
+        }
+        return false;
     }
 }
 
@@ -88,4 +132,156 @@ impl Entity for InputRecorder {
             ui.label(&format!("{:#?}", input))  
         });
     } 
+}
+
+fn vec_contains_vec<T: PartialEq>(haystack: &[T], needle: &[T]) -> bool {
+    for window in haystack.windows(needle.len()) {
+        if window == needle {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn string_to_buttons(string: &str) -> Vec<Button> {
+    let mut buttons = Vec::new();
+    for character in string.chars() {
+        match character {
+            'a' => buttons.push(Button::A),
+            'A' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::A);
+            },
+            'b' => buttons.push(Button::B),
+            'B' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::B);
+            },
+            'c' => buttons.push(Button::C),
+            'C' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::C);
+            },
+            'd' => buttons.push(Button::D),
+            'D' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::D);
+            },
+            'e' => buttons.push(Button::E),
+            'E' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::E);
+            },
+            'f' => buttons.push(Button::F),
+            'F' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::F);
+            },
+            'g' => buttons.push(Button::G),
+            'G' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::G);
+            },
+            'h' => buttons.push(Button::H),
+            'H' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::H);
+            },
+            'i' => buttons.push(Button::I),
+            'I' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::I);
+            },
+            'j' => buttons.push(Button::J),
+            'J' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::J);
+            },
+            'k' => buttons.push(Button::K),
+            'K' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::K);
+            },
+            'l' => buttons.push(Button::L),
+            'L' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::L);
+            },
+            'm' => buttons.push(Button::M),
+            'M' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::M);
+            },
+            'n' => buttons.push(Button::N),
+            'N' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::N);
+            },
+            'o' => buttons.push(Button::O),
+            'O' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::O);
+            },
+            'p' => buttons.push(Button::P),
+            'P' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::P);
+            },
+            'q' => buttons.push(Button::Q),
+            'Q' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::Q);
+            },
+            'r' => buttons.push(Button::R),
+            'R' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::R);
+            },
+            's' => buttons.push(Button::S),
+            'S' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::S);
+            },
+            't' => buttons.push(Button::T),
+            'T' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::T);
+            },
+            'u' => buttons.push(Button::U),
+            'U' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::U);
+            },
+            'v' => buttons.push(Button::V),
+            'V' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::V);
+            },
+            'w' => buttons.push(Button::W),
+            'W' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::W);
+            },
+            'x' => buttons.push(Button::X),
+            'X' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::X);
+            },
+            'y' => buttons.push(Button::Y),
+            'Y' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::Y);
+            },
+            'z' => buttons.push(Button::Z),
+            'Z' => {
+                buttons.push(Button::RShift);
+                buttons.push(Button::Z);
+            },
+            ' ' => buttons.push(Button::Space),
+
+            _ => unreachable!("Unknown character {} ", character)
+        }
+    }
+    dbg!(&buttons);
+    return buttons;
 }
--- src/project_runner.rs
+++ src/project_runner.rs
@@ -1,4 +1,6 @@
 use jovial_engine::prelude::*;
+use jovial_engine::prelude::egui::Ui;
+use crate::input::{string_to_buttons, INPUT, Input, Modes};
 use crate::project_creator::{OpenSettings, OPEN_SETTINGS};
 use std::process::Command;
 use std::thread::{self, JoinHandle};
@@ -9,7 +11,9 @@ pub const SAVE: &str = "Save";
 pub struct ProjectRunner {
     run_handle: Option<JoinHandle<()>>,
     build_handle: Option<JoinHandle<()>>,
-    run_queued: bool
+    run_queued: bool,
+    run_shortcut: Vec<Button>,
+    build_shortcut: Vec<Button>,
 }
 
 impl ProjectRunner {
@@ -17,7 +21,9 @@ impl ProjectRunner {
         Self { 
             run_handle: None, 
             build_handle: None,
-            run_queued: false
+            run_queued: false,
+            run_shortcut: string_to_buttons(" r"),
+            build_shortcut: string_to_buttons(" b"),
         }
     }
 
@@ -58,23 +64,90 @@ impl ProjectRunner {
         }
     }
 
-    fn run(&mut self, game_state: &mut GameState) {
+    fn run(&mut self, game_state: &mut GameState) -> bool {
+        if self.can_run() == false {
+            return false;
+        }
+        let input = game_state.plugins.get_mut::<Input>(INPUT).unwrap();
+        if input.is_pressed(&self.build_shortcut, Modes::Normal) == false {
+            return false;
+        }
+
+        let input = game_state.plugins.get_mut::<Input>(INPUT).unwrap();
         let open_settings = game_state.plugins.get_mut::<OpenSettings>(OPEN_SETTINGS).unwrap();
         let open_settings_copy = open_settings.clone();
         let run_handle = thread::spawn(move || Self::cargo_run(open_settings_copy));
         self.run_handle = Some(run_handle);
         self.run_queued = false;
+
+        return true;
     }
 
-    fn build(&mut self, game_state: &mut GameState) {
+    fn build(&mut self, game_state: &mut GameState) -> bool {
+        if self.can_run() == false {
+            return false;
+        }
+
+        println!("build");
+        
         let open_settings = game_state.plugins.get_mut::<OpenSettings>(OPEN_SETTINGS).unwrap();
         let open_settings_copy = open_settings.clone();
         let build_handle = thread::spawn(move || Self::cargo_build(open_settings_copy));
         self.build_handle = Some(build_handle);
+
+        return true;
     }
 
     fn can_run(&self) -> bool {
         self.run_handle.is_none() && self.build_handle.is_none()
+    }
+
+    fn check_shortcuts(&mut self, game_state: &mut GameState) {
+        self.check_run_shortcut(game_state);
+        self.check_build_shortcut(game_state);
+    }
+
+    fn check_build_shortcut(&mut self, game_state: &mut GameState) {
+        let input = game_state.plugins.get_mut::<Input>(INPUT).unwrap();
+        if input.is_pressed(&self.build_shortcut, Modes::Normal) == false {
+            return;
+        }
+        self.build(game_state);
+    }
+
+    fn check_run_shortcut(&mut self, game_state: &mut GameState) {
+        let input = game_state.plugins.get_mut::<Input>(INPUT).unwrap();
+        if input.is_pressed(&self.run_shortcut, Modes::Normal) == false {
+            return;
+        }
+        if self.build(game_state) {
+            self.run_queued = true;
+        }
+    }
+
+    fn check_build(&mut self, ui: &mut Ui, game_state: &mut GameState) {
+        if let Some(build_handle) = &self.build_handle {
+            ui.spinner();
+            if build_handle.is_finished() == false {
+                return;
+            }
+            self.build_handle = None;
+            if self.run_queued {
+                self.run(game_state);
+            }
+        }
+    }
+
+    fn check_run(&mut self, ui: &mut Ui, game_state: &mut GameState) {
+        if let Some(handle) = &self.run_handle {
+            if handle.is_finished() {
+                self.run_handle = None;
+                game_state.plugins
+                    .get_mut::<OpenSettings>(OPEN_SETTINGS)
+                    .unwrap()
+                    .has_run = true;
+            }
+        }
     }
 }
 
@@ -91,38 +164,19 @@ impl Entity for ProjectRunner {
     fn update(&mut self, game_state: &mut GameState, _entity_commands: &mut EntityCommands) {
         egui::Window::new("Project").show(&game_state.ui.ctx(), |ui| {
             ui.horizontal(|button_ui| {
-                if button_ui.button("Run").clicked() && self.can_run() {
+                if button_ui.button("Run").clicked() {
+                    if self.build(game_state) {
+                        self.run_queued = true;
+                    }
+                } else if button_ui.button("Build").clicked() {
                     self.build(game_state);
-                    self.run_queued = true;
-                }
-                if button_ui.button("Build").clicked() && self.can_run() {
-                    self.build(game_state);
-                }
-                if button_ui.button("Save").clicked() {
+                } else if button_ui.button("Save").clicked() {
                     game_state.events.add(SAVE, true)
                 }
+                self.check_shortcuts(game_state);
             });
-
-
-            if let Some(build_handle) = &self.build_handle {
-                ui.spinner();
-                if build_handle.is_finished() == false {
-                    return;
-                }
-                self.build_handle = None;
-                if self.run_queued {
-                    self.run(game_state);
-                }
-            }
-            if let Some(handle) = &self.run_handle {
-                if handle.is_finished() {
-                    self.run_handle = None;
-                    game_state.plugins
-                        .get_mut::<OpenSettings>(OPEN_SETTINGS)
-                        .unwrap()
-                        .has_run = true;
-                }
-            }
+            self.check_build(ui, game_state);
+            self.check_run(ui, game_state);
         });
     }
 }
